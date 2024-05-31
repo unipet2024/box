d0cd7d93e2b54665fa4ec8b60c40affa9b26ab78
Ta Ba Trung <trungtaba@gmail.com>
2024-05-30 22:15:01 -0400

    add script claim

--- script/claim.ts
+++ script/claim.ts
@@ -1,86 +1,40 @@
-import * as anchor from "@coral-xyz/anchor";
-import { Program, AnchorProvider } from "@coral-xyz/anchor";
-import { IDL } from "../target/types/box_2024";
-import { Wallet } from "@coral-xyz/anchor";
-
-import data from "../keys/dev/holder.json";
-
-// import { setTimeout } from "timers/promises";
-
-import { PublicKey, Keypair, Connection, clusterApiUrl } from "@solana/web3.js";
 import { getAssociatedTokenAddress } from "@solana/spl-token";
-const connection = new Connection(clusterApiUrl("devnet"), "confirmed");
-
-const wallet = Keypair.fromSecretKey(
-  Uint8Array.from([
-    60, 31, 216, 134, 68, 78, 5, 54, 175, 135, 221, 227, 168, 70, 131, 114, 133,
-    65, 139, 93, 195, 126, 28, 32, 17, 15, 252, 196, 1, 237, 44, 57, 8, 134, 50,
-    123, 56, 199, 184, 99, 61, 162, 196, 68, 143, 51, 117, 64, 26, 54, 84, 218,
-    154, 157, 209, 231, 34, 3, 251, 190, 216, 153, 90, 113,
-  ])
-);
-console.log("Wallet:", wallet.publicKey.toString());
-
-new Wallet(wallet);
-
-const provider = new AnchorProvider(
-  connection,
-  new Wallet(wallet),
-  anchor.AnchorProvider.defaultOptions()
-);
-// console.log("Provider: ", provider);
-
-const idl = IDL;
-// Address of the deployed program.
-const programId = "FYh8FdzipaoA5hWFH4P2UEiGYmgFbB1A35ELWjBLvo1f";
-// Generate the program client from IDL.
-const program = new anchor.Program(idl, programId, provider);
-
-const address0 = new PublicKey("11111111111111111111111111111111");
+import { getBoxAccount, program, provider } from "./helper";
+import { PublicKey } from "@solana/web3.js";
+import * as anchor from "@coral-xyz/anchor";
+// const address0 = new PublicKey("11111111111111111111111111111111");
 // const usdc = new PublicKey("BUJST4dk6fnM5G3FnhTVc3pjxRJE7w2C5YL9XgLbdsXW");
 
 async function claim() {
-  let owner = provider.wallet as Wallet;
-  const payer = owner.payer;
-  // Configure the client to use the local cluster.
-  anchor.setProvider(provider);
-
-  const claimer = new PublicKey("7q66w6j8oWnctRjkxDFuP6h5YU3LpsKbDUAtpf7eTnRo");
-  const MINT = new PublicKey("CJJfJQThGGYiTX4Lhyo4C2qvHZWnXGUWRgnMn9U54bVV");
+  const claimer = new PublicKey("GeYNaiuvHU3aq4WUhGcR5W1kYsdUcudUgNqJVt5aM5tv");
+  const MINT = new PublicKey("H3G1zaoUyS6bi1JGLYmaCR29utH2xKUoEXdhzdDACauS");
 
   const buyer_account = getBuyerAccount(claimer.toBuffer());
-  const box_account = getBoxAccount(4);
+  // const buyer_account_info
+  const box_account = getBoxAccount(1);
 
-  const nft_box = await getAssociatedTokenAddress(MINT, box_account);
-  console.log("NFT BOX:", nft_box);
+  const nft_box = await getAssociatedTokenAddress(MINT, box_account, true);
+  console.log("NFT BOX:", nft_box.toString());
 
-  // const unipet_box_account = getUnipetBoxAccount();
-  // const admin_account = getAdminAccount();
-  // const operator_account = getOperatorAccount();
-
-  // const starttime = Math.floor(new Date().getTime() / 1000);
-  // const endtime = starttime + 30 * 86400;
-  // const rates = [0, 50, 90, 100];
-  // const box1_name = "BOX 1";
-  // const price = 1000000;
+  const nft_buyer = await getAssociatedTokenAddress(MINT, claimer);
+  console.log("NFT Buyer:", nft_buyer.toString());
 
   let buyer_account_info = await program.account.userStruct.fetch(
     buyer_account
   );
 
-  console.log(buyer_account_info.boughts[0].id.toString());
-
-  // let operator_account_info = await program.account.authorityRole.fetch(
-  //   operator_account
-  // );
-  // console.log(operator_account_info);
+  console.log(buyer_account_info);
 
   // try {
   //   await program.methods
-  //     .claim(4, new anchor.BN(7))
+  //     .claim(1, new anchor.BN(25))
   //     .accounts({
   //       boxAccount: box_account,
-  //       nftBox:
+  //       nftBox: nft_box,
+  //       buyerAccount: buyer_account,
+  //       nftBuyer: nft_buyer,
+  //       mint: MINT,
+  //       buyer: claimer
   //     })
   //     .rpc();
   // } catch (error) {
@@ -88,51 +42,9 @@ async function claim() {
   // }
 }
 
-const getBoxAccount = (id) => {
-  const BOX_ACCOUNT = "BOX_ACCOUNT";
-  const [box_account] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(BOX_ACCOUNT), new anchor.BN(id).toArrayLike(Buffer, "le", 1)],
-    program.programId
-  );
-  console.log("box account: ", box_account.toString());
-
-  return box_account;
-};
-
-const getUnipetBoxAccount = () => {
-  const UNIPET_BOX_ACCOUNT = "UNIPET_BOX_ACCOUNT";
-  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(UNIPET_BOX_ACCOUNT)],
-    program.programId
-  );
-  console.log("unipet_box_account: ", mint.toString());
-  return mint;
-};
-
-const getOperatorAccount = () => {
-  const OPERATOR_ROLE = "OPERATOR_ROLE";
-  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(OPERATOR_ROLE)],
-    program.programId
-  );
-  console.log("operator_account: ", mint.toString());
-  return mint;
-};
-
-const getAdminAccount = () => {
-  const ADMIN_ROLE = "ADMIN_ROLE";
-  const [mint] = anchor.web3.PublicKey.findProgramAddressSync(
-    [Buffer.from(ADMIN_ROLE)],
-    program.programId
-  );
-  console.log("admin_account: ", mint.toString());
-
-  return mint;
-};
-
 const getBuyerAccount = (buyer) => {
   const USER_ACCOUNT = "USER_ACCOUNT";
-  const [buyer_account] = anchor.web3.PublicKey.findProgramAddressSync(
+  const [buyer_account] = PublicKey.findProgramAddressSync(
     [Buffer.from(USER_ACCOUNT), buyer],
     program.programId
   );
